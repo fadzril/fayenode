d52540e3c88ead473421c1efd6236a2630a33e5a
Fadzril Muhamad <fadzril@jomcode.com>
2011-07-12 11:33:11 +0800

    try binding to port 8080

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ bayeux.addExtension(serverAuth);
 /*******************************************
  * Routes
  ******************************************/
-var port = process.env.port || 3000;
+var port = process.env.port || 8080;
 
 app.get('/', function(req, res){
   res.render('index', {
